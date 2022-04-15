05217d4818bdda9b1e5e9c38e3777a16b6a0be08
choikyungsoo <81173010+choikyungsoo@users.noreply.github.com>
2022-04-15 21:53:14 +0900

    JQuery 추가

---
+++ JavaScript/main.js
@@ -0,0 +1,28 @@
+var mainProfile;
+var Fadein_Anim;
+var opacity = 0;
+var intervalID = 0;
+
+function MainProfileEvent(){
+    mainProfile = document.getElementById("mainProfile");
+    Fadein_Anim = setInterval(show, 200);
+}
+
+function show(){
+    mainProfile = document.getElementById("mainProfile");
+    opacity = Number(window.getComputedStyle(mainProfile).getPropertyValue("opacity"));
+    if(opacity<1){
+        //Fade in 핵심 부분
+        opacity = opacity+0.1;
+        mainProfile.style.opacity=opacity;
+        //img.style.opacity=opacity;
+    }
+    else{
+        clearInterval(intervalID);
+    }
+}
+
+window.onload = function(){
+    setTimeout(MainProfileEvent(), 500);
+    Fadein_Anim = setInterval(show, 200);
+}
